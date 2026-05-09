f4d32e21658d93c0a9d3ed592689f54c6d230940
eminkerimov <68128787+eminkerimov@users.noreply.github.com>
2026-05-09 19:28:25 +0300

    Change image URL in Slider component
    Updated image URL in the Slider component.

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,7 +8,7 @@ const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const data = [
-    "https://cdn.shopify.com/s/files/1/0310/3571/9739/articles/17-best-male-model-poses-for-photoshoots-956272.jpg?v=1597172634",
+    "https://images.unsplash.com/photo-1713256683892-5bab22f76be0?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2022/03/young-fashionable-couple-against-blue-background.jpg",
   ];
