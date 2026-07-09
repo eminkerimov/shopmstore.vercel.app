fd09cca2b0490d67915e5b5c5f357ef5be8ed861
Emin <eminkerimovdev@gmail.com>
2026-07-09 17:40:34 +0400

    header changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,81 +1,107 @@
-import React, { useEffect } from "react";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import SearchIcon from "@mui/icons-material/Search";
-import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
-import FavouriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import React, { useEffect, useState } from "react";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { Link } from "react-router-dom";
-import "./Navbar.scss"
+import { Link, NavLink, useLocation } from "react-router-dom";
+import "./Navbar.scss";
 import Cart from "../Cart/Cart";
-import {useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { handleCart } from "../../redux/cartReducer";
-import { useLocation } from "react-router-dom";
-
 
 const Navbar = () => {
-  const products = useSelector(state=>state.cart.products);
-  const isCartOpen = useSelector(state=>state.cart.cartOpen);
+  const products = useSelector((state) => state.cart.products);
+  const isCartOpen = useSelector((state) => state.cart.cartOpen);
   const location = useLocation().pathname;
-
   const dispatch = useDispatch();
 
+  const [isScrolled, setIsScrolled] = useState(false);
+
   useEffect(() => {
-      dispatch(handleCart(false))
+    dispatch(handleCart(false));
   }, [location, dispatch]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToFooter = () => {
+    const footer = document.querySelector(".footer");
+
+    if (footer) {
+      footer.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="wrapper">
         <div className="mobilBtn">
-        </div>
-        <div className="left">
-          <div className="item">
-            <img src="/img/flag.png" alt="flag" />
-            <KeyboardArrowDownIcon />
-          </div>
-          <div className="item">
-            <span>USD</span>
-            <KeyboardArrowDownIcon />
-          </div>
-          <div className="item underline">
-            <Link className="link" to="/products/1">Women</Link>
-          </div>
-          <div className="item underline">
-            <Link className="link" to="/products/2">Men</Link>
-          </div>
-          <div className="item underline">
-            <Link className="link" to="/products/3">Children</Link>
-          </div>
-        </div>
-        <div className="center">
-            <Link className="link" to="/">M-store</Link>
-        </div>
-        <div className="right">
-        <div className="item underline">
-            <Link className="link" to="/">Home</Link>
-        </div>
-        <div className="item underline">
-            <Link className="link" to="/about">About</Link>
-        </div>
-        <div className="item underline">
-            <Link className="link" to="/">Contact</Link>
-        </div>
-        <div className="icons">
-            <SearchIcon/>
-            <PersonOutlineOutlinedIcon/>
-            <FavouriteBorderOutlinedIcon/>
-            <div 
-            className="cartIcon"
-            onClick={() => dispatch(handleCart(!isCartOpen))
-            }>
-                <ShoppingCartOutlinedIcon/>
-                <span>{products?.length}</span>
-            </div>
-        </div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
 
+        <div className="left">
+          <div className="item underline">
+            <NavLink className="link" to="/products/1">
+              Women
+            </NavLink>
+          </div>
+
+          <div className="item underline">
+            <NavLink className="link" to="/products/2">
+              Men
+            </NavLink>
+          </div>
+
+          <div className="item underline">
+            <NavLink className="link" to="/products/3">
+              Children
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="center">
+          <Link className="link" to="/">
+            M-store
+          </Link>
+        </div>
+
+        <div className="right">
+          <div className="item underline">
+            <NavLink className="link" to="/">
+              Home
+            </NavLink>
+          </div>
+
+          <div className="item underline">
+            <NavLink className="link" to="/about">
+              About
+            </NavLink>
+          </div>
+
+          <button className="item underline contactBtn" onClick={scrollToFooter}>
+            Contact
+          </button>
+
+          <div className="icons">
+            <div
+              className="cartIcon"
+              onClick={() => dispatch(handleCart(!isCartOpen))}
+            >
+              <ShoppingCartOutlinedIcon />
+              <span>{products?.length}</span>
+            </div>
+          </div>
+        </div>
       </div>
-      {isCartOpen && <Cart/>}
+
+      {isCartOpen && <Cart />}
     </div>
   );
 };
